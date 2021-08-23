59dec057d00c31e5c05bf033506179633a51cd14
WELSH_CORGI_PC\user <sijm@naver.com>
2021-08-24 00:31:45 +0900

    JPA JOIN Many to one

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,5 +1,41 @@
 package com.cos.blog.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
 public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+	private int id;
+	
+	@Column(nullable = false,length = 100)
+	private String title;
+	
+	@Lob //대용량 데이터
+	private String content; //섬머노트 라이브러리 <html> 태그가 포함되어서 디자인 됨.
+	
+	@ColumnDefault("0")
+	private int count; //조회수
+	
+	//사용자..
+	@ManyToOne  //Many = Board User = one   한명은 여러 게시글(Board)을 작성할 수 있다..
+	@JoinColumn(name="userId")
+	private User user;  //db는 오브젝트를 저장할 수없다. FK 자바는 오브젝트를 저장할 수 있다.
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 
 }
