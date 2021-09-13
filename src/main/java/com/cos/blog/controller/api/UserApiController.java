80d012978f4a892783480dd5d909c74a174a291a
DESKTOP-N92VSJQ\Administrator <sijm@naver.com>
2021-09-13 20:07:04 +0900

    dd

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,13 +1,22 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
@@ -30,8 +39,15 @@ public class UserApiController {
 	}
 	
 	@PutMapping("/user")
-	public ResponseDto<Integer> update(@RequestBody User user){
+	public ResponseDto<Integer> update(@RequestBody User user,@AuthenticationPrincipal PrincipalDetail principal,HttpSession session){
 		userService.회원수정(user);
+		// db는 변경 되었지만
+		// 세션값은 변경이 안됨
+		Authentication authentication = 
+				new UsernamePasswordAuthenticationToken(principal, null);
+		SecurityContext securityContext= SecurityContextHolder.getContext();
+		securityContext.setAuthentication(authentication);
+		session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 	}
 	
