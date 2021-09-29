adbcbef2876ffcc920f939c6533e72868a0f771a
behong-pc <sijm@naver.com>
2021-09-30 01:42:41 +0900

    restTemplete 테스트 코드

---
+++ src/main/java/com/cos/blog/restTemplete/TestController.java
@@ -0,0 +1,39 @@
+package com.cos.blog.restTemplete;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.blog.restTemplete.model.MemberDTO;
+import com.cos.blog.restTemplete.service.RestTempleteService;
+import com.cos.blog.service.BoardService;
+
+@Controller
+public class TestController {
+	
+ @Autowired
+ RestTempleteService restTempleteService;
+ 
+ @GetMapping("/getTest1")
+ public String getTest1() {
+	 return restTempleteService.getTest();
+ }
+
+ 
+ @GetMapping("/getMember")
+ public ResponseEntity<MemberDTO> getMember() {
+	 return restTempleteService.getMember();
+ }
+
+ @GetMapping("/addHeader")
+ public ResponseEntity<MemberDTO> addHeader() {
+	 return restTempleteService.addHeader();
+ }
+ 
+}
---
+++ src/main/java/com/cos/blog/restTemplete/service/RestTempleteService.java
@@ -0,0 +1,102 @@
+package com.cos.blog.restTemplete.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.cos.blog.restTemplete.model.MemberDTO;
+
+import java.net.URI;
+
+@Service
+public class RestTempleteService {
+	
+	private final Logger LOGGER= LoggerFactory.getLogger(RestTempleteService.class);
+	
+	public String getTest() {
+		LOGGER.info("getTest start :{} " );
+		URI  uri = UriComponentsBuilder
+				.fromUriString("http://localhost:8833")
+				.path("/api/server/test1")
+				.encode()
+				.build()
+				.toUri();
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+		
+		LOGGER.info("status code :{} ", responseEntity.getStatusCode()   );
+		LOGGER.info("body :{} ", responseEntity.getBody()   );
+		
+		return responseEntity.getBody();
+	}
+	
+	public ResponseEntity<MemberDTO> getMember() {
+		LOGGER.info("getMember start :{} " );
+		
+		URI  uri = UriComponentsBuilder
+				.fromUriString("http://localhost:8833")
+				.path("/api/server/getMember")
+				.queryParam("name", "지유소유")
+				.queryParam("email", "jisou@email.com")
+				.queryParam("organnization", "jusou")
+				.encode()
+				.build()
+				.toUri();		
+		
+		//dto도 전달할 수 있다.
+		MemberDTO mem = new MemberDTO();
+		mem.setName("아빠");
+		mem.setEmail("father@email.com");
+		mem.setOrganization("nayha");
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<MemberDTO> responseEntity = restTemplate.postForEntity(uri, mem, MemberDTO.class);
+
+		LOGGER.info("status code :{} ", responseEntity.getStatusCode()   );
+		LOGGER.info("body :{} ", responseEntity.getBody()   );
+		
+		return responseEntity;
+	}
+	
+	public ResponseEntity<MemberDTO> addHeader() {
+		LOGGER.info("addHeader start :{} " );
+		
+		URI  uri = UriComponentsBuilder
+				.fromUriString("http://localhost:8833")
+				.path("/api/server/addHeader")
+				.queryParam("name", "지유소유")
+				.queryParam("email", "jisou@email.com")
+				.queryParam("organnization", "jusou")
+				.encode()
+				.build()
+				.toUri();		
+		
+		
+		MemberDTO mem = new MemberDTO();
+		mem.setName("아빠");
+		mem.setEmail("father@email.com");
+		mem.setOrganization("nayha");
+		
+		//헤더 생성
+		RequestEntity<MemberDTO> requestEntity = RequestEntity
+												 .post(uri)
+												 .header("customHeader", "커스텀 헤더")
+												 .body(mem);
+				
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<MemberDTO> responseEntity = restTemplate.exchange(requestEntity, MemberDTO.class);
+
+		LOGGER.info("status code :{} ", responseEntity.getStatusCode()   );
+		LOGGER.info("body :{} ", responseEntity.getBody()   );
+		
+		return responseEntity;
+	}	
+}
